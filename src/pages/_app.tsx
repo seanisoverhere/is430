dde59fee5feb761ecc943203785cff1dbb8586bb
Sean Choon <38978508+seanisoverhere@users.noreply.github.com>
2022-10-12 08:38:10 +0800

    feat: setup tailwind and next head (#2)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>IS430 B2B</title>
+        <meta name="description" content="IS430 B2B Lending App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
