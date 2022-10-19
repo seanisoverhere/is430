170af5cb58dbdc1ad579c6bb8e2d19ccd78f5694
Sean Choon <sean_choon@hotmail.com>
2022-10-19 09:25:55 +0800

    test OTP components

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,19 @@
 import type { NextPage } from "next";
+import OTP from "@/components/Login/OTP";
 
 const Login: NextPage = () => {
-  return <div>Login Page</div>;
+  return (
+    <div className='bg-black min-h-screen'>
+      Login Page
+      <OTP
+        autoFocus
+        length={6}
+        className="flex justify-between items-center"
+        inputClassName="w-14 h-14 text-4xl text-center"
+        onChangeOTP={(otp) => console.log(otp)}
+      />
+    </div>
+  );
 };
 
 export default Login;
