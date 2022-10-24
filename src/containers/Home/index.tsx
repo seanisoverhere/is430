735e630efca4a70ddd4e22b53c46f3158120113f
Sean Choon <sean_choon@hotmail.com>
2022-10-24 20:59:31 +0800

    add style for dashboard cards

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,14 +1,67 @@
 import React, { useState } from "react";
-import { Layout } from "antd";
+import { Layout, Row, Col, Typography } from "antd";
+import { CarOutlined } from "@ant-design/icons";
+import { StyledCard, IconBox, Icon } from "./styles";
 
 const { Content } = Layout;
+const { Title } = Typography;
+
+const count = [
+  {
+    today: "Today's Sales",
+    title: "$53,000",
+    icon: <CarOutlined />,
+  },
+  {
+    today: "Today's Users",
+    title: "3,200",
+    icon: <CarOutlined />,
+  },
+  {
+    today: "New Clients",
+    title: "+1,200",
+    icon: <CarOutlined />,
+  },
+  {
+    today: "New Orders",
+    title: "$13,200",
+    icon: <CarOutlined />,
+  },
+];
 
 const Home = () => {
   return (
     <Layout>
       <Content>
         <div className="p-12">
-          <div className="text-2xl font-bold">Dashboard</div>
+          <div className="text-2xl font-bold mb-8">Dashboard</div>
+          <Row gutter={[24, 0]}>
+            {count.map((c, index) => (
+              <Col
+                key={index}
+                xs={24}
+                sm={24}
+                md={24}
+                lg={24}
+                xl={6}
+                className="mb-9"
+              >
+                <StyledCard bordered={false}>
+                  <div className="flex justify-between items-center w-full">
+                    <div className='sm:text-base lg:text-lg'>
+                      <span>{c.today}</span>
+                      <div className="font-bold">
+                        {c.title}
+                      </div>
+                    </div>
+                    <IconBox>
+                      <Icon>{c.icon}</Icon>
+                    </IconBox>
+                  </div>
+                </StyledCard>
+              </Col>
+            ))}
+          </Row>
         </div>
       </Content>
     </Layout>
---
+++ src/containers/Home/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { Card } from "antd";
+
+export const StyledCard = styled(Card)`
+  && {
+    border-radius: 20px;
+  }
+`;
+
+export const IconBox = styled.div`
+  width: 48px;
+  height: 48px;
+  text-align: center;
+  background: #1890ff;
+  color: #fff;
+  border-radius: 0.5rem;
+`;
+
+export const Icon = styled.div`
+  height: 100%;
+  font-size: 1.5rem;
+`;
