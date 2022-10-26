e6ee48c20ee59a8eab770d35d0dfc5ecd649b762
Sean Choon <sean_choon@hotmail.com>
2022-10-26 10:20:05 +0800

    refactor login page

---
+++ src/containers/Login/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginForm from "@/components/Login/LoginForm";
+import { LoginContainer } from "./styles";
+
+const Login = () => {
+  return (
+    <LoginContainer>
+      <LoginForm />
+    </LoginContainer>
+  );
+};
+
+export default Login;
