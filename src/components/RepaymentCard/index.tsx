404f63aaab36975d0f9d4ceaeb1fbab21133712a
Sean Choon <sean_choon@hotmail.com>
2022-10-27 00:11:22 +0800

    add hover animation

--- src/components/RepaymentCard/index.tsx
+++ src/components/RepaymentCard/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Card } from "antd-mobile";
+import { StyledCard } from "./styles";
 
 type RepaymentCardProps = {
   title: string;
 };
 
 const RepaymentCard = ({ title }: RepaymentCardProps) => {
-  return <Card title={title}>Hello</Card>;
+  return (
+    <StyledCard title={title} extra="$1522,22">
+      Hello
+    </StyledCard>
+  );
 };
 
 export default RepaymentCard;
