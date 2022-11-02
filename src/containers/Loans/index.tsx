a3a98efc095dcc043df72706979e644d78ceae14
Sean Choon <sean_choon@hotmail.com>
2022-11-02 19:33:15 +0800

    add supplier option

--- src/containers/Loans/index.tsx
+++ src/containers/Loans/index.tsx
@@ -13,11 +13,16 @@ import {
   StyledSelect,
 } from "./styles";
 import { Col } from "antd";
+import { supplierOptions } from "@/utils/constants/suppliers";
+import { InstructionText } from "../Home/styles";
+
+const { Option } = StyledSelect;
 
 const Loans = () => {
   const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
   const [amountInput, setAmountInput] = useState<number>(0);
   const [hasError, setHasError] = useState<boolean>(false);
+  const [uen, setUen] = useState<string>();
 
   const checkLoanLogic = (amount?: string) => {
     if (Number(amount) > 300000) {
@@ -32,17 +37,34 @@ const Loans = () => {
 
   const submitLoan = () => [console.log(Number(amountInput))];
 
+  const handleOnChange = (value: any) => {
+    console.log(value);
+    setUen(value);
+  };
+
   return (
     <>
       <PageTitle>Loans</PageTitle>
+      <InstructionText>
+        Please enter the amount you would like to borrow, you are only limited
+        to <strong>S$300,000</strong> per loan instance. Please note that it
+        will take <strong>3-5 working days</strong> for us to verify and approve
+        the loan
+      </InstructionText>
       <StyledRow justify="space-between">
         <Col span={11}>
           <InputText>Supplier:</InputText>
-          <StyledSelect bordered={false} />
+          <StyledSelect bordered={false} onChange={handleOnChange}>
+            {Object.entries(supplierOptions).map(([key, value]) => (
+              <Option key={value} value={value}>
+                {key}
+              </Option>
+            ))}
+          </StyledSelect>
         </Col>
         <Col span={11}>
           <InputText>UEN</InputText>
-          <StyledInput type="text" />
+          <StyledInput type="text" value={uen} />
         </Col>
       </StyledRow>
       <CurrencyContainer>
