5c46cb7d50e968f16407135d8fc425d8ca15acb0
alvinangjh <alvinangjh@gmail.com>
2022-11-02 20:51:25 +0800

    individual bills

---
+++ src/pages/api/bill/[billId].ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/prisma";
+
+type Data = {
+    data?: string;
+    message: string;
+    success: boolean;
+};
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    if (req.method === "GET") {
+        return await retrieveAllLoans(req, res);
+    }
+
+    return res
+        .status(405)
+        .json({ message: "Method not allowed", success: false });
+}
+
+const retrieveAllLoans = async (
+    req: NextApiRequest,
+    res: NextApiResponse
+) => {
+    let { billId } = req.query
+
+    const splitBills = prisma.$queryRaw`SELECT * 
+    FROM paymentSplit ps
+    WHERE ps.mainPaymentId = 4; ${billId};`
+
+
+    return res.status(200).json({
+        individualBills: splitBills,
+    })
+}
