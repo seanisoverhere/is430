a40d23050eb3a83bd99b8856e11109e7b29e6e2f
alvinangjh <alvinangjh@gmail.com>
2022-11-03 01:15:48 +0800

    algo revised

--- src/pages/api/industry/score.ts
+++ src/pages/api/industry/score.ts
@@ -1,4 +1,3 @@
-//these can be inserted into db instead
 const SECTOR_VOLATILITY: any = {
     basicMaterial: -0.0024,
     utilities: 0.0216,
@@ -13,18 +12,9 @@ const SECTOR_VOLATILITY: any = {
 
 export default async function calIndustryScore(industry: string) {
     let industryScore = SECTOR_VOLATILITY[industry]
-    console.log("INDUSTRY START")
-    console.log(industryScore)
-
     const maxIndustryScore = Math.max.apply(null, Object.values(SECTOR_VOLATILITY))
-    console.log(maxIndustryScore)
     const minIndustryScore = Math.min.apply(null, Object.values(SECTOR_VOLATILITY))
-    console.log(minIndustryScore)
     let industryDelta = maxIndustryScore - industryScore
-    console.log(industryDelta)
-
     let normalizedIndustryScore = industryDelta / (maxIndustryScore - minIndustryScore)
-    console.log(normalizedIndustryScore)
-    console.log("INDUSTRY END")
     return normalizedIndustryScore
 }
