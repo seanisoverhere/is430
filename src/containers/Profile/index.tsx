7b789a95cc98afdc8d68347182094f91eaf28343
Sean Choon <sean_choon@hotmail.com>
2022-11-03 15:31:14 +0800

    add profile page

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import { PageTitle } from "@/utils/styles";
+import {
+  FlexContainer,
+  CompanyName,
+  CompanyUen,
+  InstructionText,
+  FormContainer,
+} from "./styles";
+import { useForm } from "react-hook-form";
+import FormInput from "@/components/FormInput";
 
 const Profile = () => {
   return (
     <>
       <PageTitle>Profile</PageTitle>
+      <FlexContainer>
+        <CompanyName>Dog PTE LTD</CompanyName>
+        <CompanyUen>UEN: ABC1223</CompanyUen>
+      </FlexContainer>
+      <InstructionText>
+        We will prompt you <strong>quarterly</strong> to update your company
+        financial health with us for a{" "}
+        <strong>re-assessment of your credit score</strong>. Alternatively, you
+        may also update at any point in time. We will update you on your credit
+        score within <strong>3-5 working days</strong>.
+      </InstructionText>
+      <FormContainer>
+        <form onSubmit={() => console.log("submit")}>hello form</form>
+      </FormContainer>
     </>
   );
 };
---
+++ src/containers/Profile/styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const FlexContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 1rem 0;
+`;
+
+export const CompanyName = styled.div`
+  font-size: 1.1rem;
+  font-weight: 600;
+`;
+
+export const CompanyUen = styled.div`
+  font-size: 0.9rem;
+  color: #999;
+`;
+
+export const InstructionText = styled.div`
+  margin: 1rem 0 2rem;
+  font-size: 0.9rem;
+`;
+
+export const FormContainer = styled.div`
+  margin: 1rem 0;
+`;
