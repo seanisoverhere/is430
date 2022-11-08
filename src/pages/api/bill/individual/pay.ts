9c971b79de9fe67808255c2dc568b94f6547522e
alvinangjh <alvinangjh@gmail.com>
2022-11-08 21:56:23 +0800

    temp

--- src/pages/api/bill/individual/pay.ts
+++ src/pages/api/bill/individual/pay.ts
@@ -25,39 +25,38 @@ const pay = async (
     res: NextApiResponse
 ) => {
 
+    // const header = {
+    //     Header: {
+    //         'serviceName': 'creditTransfer',
+    //         'userID': 'DEUT0000001',
+    //         'PIN': '123456',
+    //         'OTP': '999999',
+    //     }
+    // }
 
-    const header = {
-        Header: {
-            'serviceName': 'creditTransfer',
-            'userID': 'DEUT0000001',
-            'PIN': '123456',
-            'OTP': '999999',
-        }
-    }
+    // const content = {
+    //     Content: {
+    //         'accountFrom': String(data.mainPayment?.payerAcctId),
+    //         'accountTo': String(data.mainPayment?.invoice?.receiverAcctId),
+    //         'transactionAmount': String(data.paymentAmount),
+    //         'transactionReferenceNumber': String(data.splitLoanId),
+    //         'narrative': 'Credit Transfer',
+    //     }
+    // }
 
-    const content = {
-        Content: {
-            'accountFrom': String(data.mainPayment?.payerAcctId),
-            'accountTo': String(data.mainPayment?.invoice?.receiverAcctId),
-            'transactionAmount': String(data.paymentAmount),
-            'transactionReferenceNumber': String(data.splitLoanId),
-            'narrative': 'Credit Transfer',
-        }
-    }
+    // const jsonHeader = JSON.stringify(header)
+    // const jsonContent = JSON.stringify(content)
 
-    const jsonHeader = JSON.stringify(header)
-    const jsonContent = JSON.stringify(content)
+    // const creditTransfer: any = await fetch(`http://tbankonline.com/SMUtBank_API/Gateway?Header=${jsonHeader}&Content=${jsonContent}`, {
+    //     method: 'POST',
+    // })
 
-    const creditTransfer: any = await fetch(`http://tbankonline.com/SMUtBank_API/Gateway?Header=${jsonHeader}&Content=${jsonContent}`, {
-        method: 'POST',
-    })
-
-    if (creditTransfer.Content.ServiceResponse.ServiceRespHeader.GlobalErrorID !== "010000") {
-        return res.status(400).json({
-            message: "Payment update failed",
-            success: false
-        })
-    }
+    // if (creditTransfer.Content.ServiceResponse.ServiceRespHeader.GlobalErrorID !== "010000") {
+    //     return res.status(400).json({
+    //         message: "Payment update failed",
+    //         success: false
+    //     })
+    // }
 
     const data = await prisma.paymentSplit.update({
         where: {
